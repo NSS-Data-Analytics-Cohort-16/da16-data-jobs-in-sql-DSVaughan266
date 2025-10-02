b9b47fe34ae31d892c659e37579c9684130bc570
DSVaughan266 <dsvaughan@yahoo.com>
2025-10-02 18:35:47 -0500

    Finished with Bonus

--- Scripts/data_analyst_jobs_exercise.sql
+++ Scripts/data_analyst_jobs_exercise.sql
@@ -23,6 +23,11 @@ LIMIT 10;
 SELECT COUNT(location)
 FROM data_analyst_jobs
 WHERE location = 'TN';
+
+SELECT
+	COUNT(location)
+FROM data_analyst_jobs
+WHERE location = 'TN';
 --ANSWER:	21
 
 SELECT COUNT(location)
@@ -43,8 +48,8 @@ WHERE location = 'TN' AND star_rating > 4;
 
 SELECT COUNT(review_count)
 FROM data_analyst_jobs
-WHERE review_count > 500 AND review_count < 1000;
---ANSWER:	150
+WHERE review_count >= 500 AND review_count <= 1000;
+--ANSWER:	151
 
 -- 6.	Show the average star rating for companies in each state. 
 --		The output should show the state as `state` and the average rating for the state as `avg_rating`. 
@@ -56,7 +61,7 @@ SELECT
 FROM data_analyst_jobs
 GROUP BY location 
 ORDER BY avg_rating DESC;
---ANSWER:	NE
+--ANSWER:	Nebraska
 
 -- 7.	Select unique job titles from the data_analyst_jobs table. How many are there?
 
@@ -87,7 +92,7 @@ SELECT
 FROM data_analyst_jobs
 WHERE review_count >5000
 AND company IS NOT NULL
-GROUP BY company
+GROUP BY company;
 --ANSWER:	40
 
 -- 10.	Add the code to order the query in #9 from highest to lowest average star rating. 
@@ -101,7 +106,7 @@ FROM data_analyst_jobs
 WHERE review_count >5000
 AND company IS NOT NULL
 GROUP BY company
-ORDER BY AVG(star_rating) DESC
+ORDER BY AVG(star_rating) DESC;
 --ANSWER: 6 Companies have a 4.20 rating
 --	      Unilever, General Motors, Nike, American Express, Microsoft, Kaiser Permanente
 		
@@ -110,7 +115,7 @@ ORDER BY AVG(star_rating) DESC
 SELECT
 	DISTINCT title
 FROM data_analyst_jobs
-WHERE title iLIKE '%Analyst%'
+WHERE title iLIKE '%Analyst%';
 --ANSWER: 774
 
 -- 12.	How many different job titles do not contain either the word ‘Analyst’ or the word ‘Analytics’? 
@@ -120,10 +125,48 @@ SELECT
 	DISTINCT (title)
 FROM data_analyst_jobs
 WHERE title NOT iLIKE '%Analyst%'
-AND title NOT iLIKE '%Analytics%'
---ANSWER: 4, 
+AND title NOT iLIKE '%Analytics%';
+--ANSWER: 4, Tableau
+
 -- **BONUS:**
--- You want to understand which jobs requiring SQL are hard to fill. Find the number of jobs by industry (domain) that require SQL and have been posted longer than 3 weeks. 
---  - Disregard any postings where the domain is NULL. 
---  - Order your results so that the domain with the greatest number of `hard to fill` jobs is at the top. 
---   - Which three industries are in the top 3 on this list? How many jobs have been listed for more than 3 weeks for each of the top 3?
+-- You want to understand which jobs requiring SQL are hard to fill. 
+-- Find the number of jobs by industry (domain) that require SQL and have been posted longer than 3 weeks. 
+-- Disregard any postings where the domain is NULL.
+-- Order your results so that the domain with the greatest number of `hard to fill` jobs is at the top.
+
+SELECT
+	domain,
+	COUNT(domain) as job_count
+FROM data_analyst_jobs
+WHERE 
+	domain IS NOT NULL
+	AND skill = 'SQL'
+	AND days_since_posting > 21
+GROUP BY domain
+ORDER BY job_count DESC
+--ANSWER:	Consulting and Business Services	5
+--			Consumer Goods and Services			2
+--			Computers and Electronics			1
+--			Internet and Software				1
+--			Real Estate							1
+--			Transport and Freight				1
+
+
+-- Which three industries are in the top 3 on this list? 
+-- How many jobs have been listed for more than 3 weeks for each of the top 3?
+
+SELECT
+	domain,
+	COUNT(domain) as job_count
+FROM data_analyst_jobs
+WHERE 
+	domain IS NOT NULL
+	AND skill = 'SQL'
+	AND days_since_posting > 21
+GROUP BY domain
+ORDER BY job_count DESC
+LIMIT 3;
+--ANSWER:	Consulting and Business Services	5
+--			Consumer Goods and Services			2
+--			Computers and Electronics			1
+
